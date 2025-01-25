779e152e3b1b56d5d9e6a3f581c4598fdb6ecac8
hugueslopezpardo <hugueslopezpardo@gmail.com>
2025-01-25 19:20:59 +0100

    Message

--- resources/js/pages/Application/Welcome/Index.tsx
+++ resources/js/pages/Application/Welcome/Index.tsx
@@ -1,9 +1,13 @@
-import {Link} from '@inertiajs/react';
+import {Link, useForm} from '@inertiajs/react';
 import {PageProps} from '@/types';
 import {ArrowPathIcon} from "@heroicons/react/16/solid";
 import ApplicationLayout from "@/layouts/ApplicationLayout";
 import {BanknoteIcon, CalendarIcon} from "lucide-react";
 import AboutSection from "@/components/app/Pages/About/AboutSection";
+import {FormEventHandler, useState} from "react";
+import {useToast} from "@/hooks/use-toast";
+import {Checkbox} from "@/components/ui/checkbox";
+import {Label} from "@/components/ui/label";
 
 const collections = [
     {
@@ -42,7 +46,45 @@ const perks = [{
 
 
 const WelcomePage = ({auth, trending}: PageProps<{ trending: any[] }>) => {
-    return (<ApplicationLayout auth={auth} title="Accueil">
+
+    const [isAgreed, setIsAgreed] = useState(false);
+
+    const { toast } = useToast()
+
+    const { data, setData, post, processing, errors, reset } = useForm({
+        first_name: '',
+        last_name: '',
+        email: '',
+        message: '',
+        is_agreed: isAgreed,
+    });
+
+    const submit: FormEventHandler = (e) => {
+        e.preventDefault();
+        post(route('contact.store'), {
+            onSuccess: () => {
+                reset('first_name', 'last_name', 'email', 'message')
+                toast({
+                    title: 'Message envoyé',
+                    description: 'Votre message a bien été envoyé, nous vous répondrons dans les plus brefs délais.',
+                    duration: 3000,
+                });
+            },
+            onError: () => {
+                toast({
+                    title: 'Erreur',
+                    description: 'Une erreur s\'est produite lors de l\'envoi de votre message, veuillez réessayer.',
+                    duration: 3000,
+                    variant: 'destructive',
+                });
+            }
+        });
+    };
+
+
+
+    return (
+        <ApplicationLayout auth={auth} title="Accueil">
             <section>
                 <div className="relative">
                     {/* Background image and overlap */}
@@ -100,38 +142,39 @@ const WelcomePage = ({auth, trending}: PageProps<{ trending: any[] }>) => {
                         <h2 id="collection-heading" className="sr-only">
                             Collections
                         </h2>
-                        <div className="mx-auto grid max-w-md grid-cols-1 gap-y-6 px-4 sm:max-w-7xl sm:grid-cols-4 sm:gap-x-6 sm:gap-y-0 sm:px-6 lg:gap-x-8 lg:px-8">
+                        <div
+                            className="mx-auto grid max-w-md grid-cols-1 gap-y-6 px-4 sm:max-w-7xl sm:grid-cols-4 sm:gap-x-6 sm:gap-y-0 sm:px-6 lg:gap-x-8 lg:px-8">
                             {collections.map((collection) => (<div
-                                    key={collection.name}
-                                    className="group relative h-96 rounded-lg bg-white shadow-xl sm:aspect-h-5 sm:aspect-w-4 sm:h-auto"
-                                >
-                                    <div>
-                                        <div aria-hidden="true" className="absolute inset-0 overflow-hidden rounded-lg">
-                                            <div className="absolute inset-0 overflow-hidden">
-                                                <img
-                                                    alt={collection.alt}
-                                                    src={collection.src}
-                                                    className="h-full w-full object-contain p-8 object-center transition duration-500 ease-in-out group-hover:scale-110 group-hover:brightness-125"
-                                                />
-                                            </div>
-                                            <div
-                                                className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"/>
+                                key={collection.name}
+                                className="group relative h-96 rounded-lg bg-white shadow-xl sm:aspect-h-5 sm:aspect-w-4 sm:h-auto"
+                            >
+                                <div>
+                                    <div aria-hidden="true" className="absolute inset-0 overflow-hidden rounded-lg">
+                                        <div className="absolute inset-0 overflow-hidden">
+                                            <img
+                                                alt={collection.alt}
+                                                src={collection.src}
+                                                className="h-full w-full object-contain p-8 object-center transition duration-500 ease-in-out group-hover:scale-110 group-hover:brightness-125"
+                                            />
                                         </div>
-                                        <div className="absolute inset-0 flex items-end rounded-lg p-6">
-                                            <div>
-                                                <p aria-hidden="true" className="text-sm text-white">
-                                                    Découvrez la collection
-                                                </p>
-                                                <h3 className="mt-1 font-semibold text-white">
-                                                    <Link href={collection.href}>
-                                                        <span className="absolute inset-0"/>
-                                                        {collection.name}
-                                                    </Link>
-                                                </h3>
-                                            </div>
+                                        <div
+                                            className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-50"/>
+                                    </div>
+                                    <div className="absolute inset-0 flex items-end rounded-lg p-6">
+                                        <div>
+                                            <p aria-hidden="true" className="text-sm text-white">
+                                                Découvrez la collection
+                                            </p>
+                                            <h3 className="mt-1 font-semibold text-white">
+                                                <Link href={collection.href}>
+                                                    <span className="absolute inset-0"/>
+                                                    {collection.name}
+                                                </Link>
+                                            </h3>
                                         </div>
                                     </div>
-                                </div>))}
+                                </div>
+                            </div>))}
                         </div>
                     </section>
                 </div>
@@ -144,15 +187,15 @@ const WelcomePage = ({auth, trending}: PageProps<{ trending: any[] }>) => {
                 <div
                     className="mx-auto max-w-7xl divide-y divide-gray-200 lg:flex lg:justify-center lg:divide-x lg:divide-y-0 lg:py-8">
                     {perks.map((perk, perkIdx) => (<div key={perkIdx} className="py-8 lg:w-1/3 lg:flex-none lg:py-0">
-                            <div className="mx-auto flex max-w-xs items-center px-4 lg:max-w-none lg:px-8">
-                                <perk.icon aria-hidden="true"
-                                           className="h-8 w-8 flex-shrink-0 text-red-600"/>
-                                <div className="ml-4 flex flex-auto flex-col-reverse">
-                                    <h3 className="font-medium text-gray-900">{perk.name}</h3>
-                                    <p className="text-sm text-gray-500">{perk.description}</p>
-                                </div>
+                        <div className="mx-auto flex max-w-xs items-center px-4 lg:max-w-none lg:px-8">
+                            <perk.icon aria-hidden="true"
+                                       className="h-8 w-8 flex-shrink-0 text-red-600"/>
+                            <div className="ml-4 flex flex-auto flex-col-reverse">
+                                <h3 className="font-medium text-gray-900">{perk.name}</h3>
+                                <p className="text-sm text-gray-500">{perk.description}</p>
                             </div>
-                        </div>))}
+                        </div>
+                    </div>))}
                 </div>
             </section>
 
@@ -268,6 +311,127 @@ const WelcomePage = ({auth, trending}: PageProps<{ trending: any[] }>) => {
                         ))}
                     </div>
 
+                </div>
+            </section>
+            <section>
+                <div className="isolate bg-gray-50 px-6 py-16 sm:py-16 lg:px-8" id={'contact'}>
+                    <div className="mx-auto max-w-2xl text-center">
+                        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+                            Nous contacter
+                        </h2>
+                        <p className="mt-2 text-lg leading-8 text-gray-600">
+                            Pour toute question ou demande d'information, n'hésitez pas à nous contacter en
+                            remplissant le formulaire ci-dessous.
+                        </p>
+                    </div>
+                    <form className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={submit}>
+                        <div className="flex flex-col gap-y-6">
+                            <div>
+                                <label htmlFor="first-name"
+                                       className="block text-sm font-semibold leading-6 text-gray-900">
+                                    Prénom
+                                </label>
+                                <div className="mt-2.5">
+                                    <input
+                                        id="first-name"
+                                        name="first-name"
+                                        type="text"
+                                        autoComplete="given-name"
+                                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                                        onChange={(e) => setData('first_name', e.target.value)}
+                                        defaultValue={data.first_name}
+                                    />
+                                </div>
+                                <p className="mt-1 text-sm text-red-600">
+                                    {errors.first_name}
+                                </p>
+                            </div>
+                            <div>
+                                <label htmlFor="last-name"
+                                       className="block text-sm font-semibold leading-6 text-gray-900">
+                                    Nom
+                                </label>
+                                <div className="mt-2.5">
+                                    <input
+                                        id="last-name"
+                                        name="last-name"
+                                        type="text"
+                                        autoComplete="family-name"
+                                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                                        onChange={(e) => setData('last_name', e.target.value)}
+                                        defaultValue={data.last_name}
+                                    />
+                                </div>
+                                <p className="mt-1 text-sm text-red-600">
+                                    {errors.last_name}
+                                </p>
+                            </div>
+                            <div>
+                                <label htmlFor="email"
+                                       className="block text-sm font-semibold leading-6 text-gray-900">
+                                    Email
+                                </label>
+                                <div className="mt-2.5">
+                                    <input
+                                        id="email"
+                                        name="email"
+                                        type="email"
+                                        autoComplete="email"
+                                        className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                                        onChange={(e) => setData('email', e.target.value)}
+                                        defaultValue={data.email}
+                                    />
+                                </div>
+                                <p className="mt-1 text-sm text-red-600">
+                                    {errors.email}
+                                </p>
+                            </div>
+                            <div>
+                                <label htmlFor="message"
+                                       className="block text-sm font-semibold leading-6 text-gray-900">
+                                    Message
+                                </label>
+                                <div className="mt-2.5">
+                          <textarea
+                              id="message"
+                              name="message"
+                              rows={4}
+                              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-600 sm:text-sm sm:leading-6"
+                              onChange={(e) => setData('message', e.target.value)}
+                              defaultValue={data.message}
+                          />
+                                </div>
+                                <p className="mt-1 text-sm text-red-600">
+                                    {errors.message}
+                                </p>
+                            </div>
+                            <div className="flex items-center w-full text-sm text-gray-600 space-x-2">
+                                <Checkbox id="terms" onChange={() => setIsAgreed(!isAgreed)}
+                                          className={'text-red-600'}/>
+                                <Label htmlFor="terms" className="text-gray-600 leading-6">
+                                    En soumettant ce formulaire, j'accepte que mes informations soient utilisées{' '}
+                                    <Link href={route('privacy-policy.index')}
+                                          className="font-semibold text-red-600">
+                                        conformément à la politique de confidentialité{' '}
+                                    </Link>
+                                    et j'accepte les{' '}
+                                    <Link href={route('terms-of-service.index')}
+                                          className="font-semibold text-red-600">
+                                        conditions générales d'utilisation.
+                                    </Link>
+                                </Label>
+                            </div>
+                            <p className="mt-1 text-sm text-red-600">
+                                {errors.is_agreed}
+                            </p>
+                        </div>
+                        <div className="mt-10">
+                            <button type="submit"
+                                    className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600">
+                                J'envoie mon message
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </section>
         </ApplicationLayout>);
