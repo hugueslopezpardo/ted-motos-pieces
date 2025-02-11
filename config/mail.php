e5d2655661cc698e2dff171393f27d2e984adee7
Hugues LOPEZ-PARDO <hugues-lopez-pardo@mac-1.home>
2025-02-11 10:46:21 +0100

    Modification config

--- config/mail.php
+++ config/mail.php
@@ -14,8 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'log'),
-
+    'default' => env('MAIL_MAILER', 'sendmail'),
     /*
     |--------------------------------------------------------------------------
     | Mailer Configurations
